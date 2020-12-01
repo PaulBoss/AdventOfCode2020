2bb75be4e0b20d160f40e221bdf29813949f7a37
Paul Bosselaar <paul.bosselaar@spotler.nl>
2020-12-01 13:42:13 +0100

    Day 1, part 2

--- src/net/bosselaar/adventofcode2020/Day1Part2.java
+++ src/net/bosselaar/adventofcode2020/Day1Part2.java
@@ -6,17 +6,20 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Day1 {
+public class Day1Part2 {
 
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("./input/day1-1.txt"));
         List<Integer> numbers = lines.stream().map(Integer::parseUnsignedInt).collect(Collectors.toList());
 
-        for (int x = 0; x < numbers.size() - 1; x++) {
-            for (int i = x; i < numbers.size(); i++) {
-                if (numbers.get(i) + numbers.get(x) == 2020) {
-                    System.out.println(numbers.get(i) * numbers.get(x));
-                    return;
+        for (int x = 0; x < numbers.size() - 2; x++) {
+            for (int i = x; i < numbers.size() - 1; i++) {
+                for (int k = i; k < numbers.size(); k++) {
+
+                    if (numbers.get(i) + numbers.get(x) + numbers.get(k) == 2020) {
+                        System.out.println(numbers.get(i) * numbers.get(x)  * numbers.get(k));
+                        return;
+                    }
                 }
             }
         }
